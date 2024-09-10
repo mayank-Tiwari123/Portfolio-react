0fda50597ca15a12e12583ba46ea7c3ce97d274f
mayank-Tiwari123 <mayantiwari607@gmail.com>
2024-09-09 21:39:28 -0700

    My portfolio

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './components/NavBar'
+import Home from './components/Home'
+import About from './components/About'
+import PortFolio from './components/PortFolio'
+import Experiance from './components/Experiance'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
+
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Home></Home>
+    <About></About>
+    <PortFolio></PortFolio>
+    <Experiance></Experiance>
+    <Footer></Footer>
+    <Contact></Contact>
+
+    </>
+  )
+    
+  
+}
+export default App
---
+++ src/components/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Mayank Tiwari, a passionate Web developer with a keen eye for Full
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+         <b> [ Degree/Course: M C A], [Universrity: A K T U Lucknow Uttar Pradesh India], [Year: 2024], [Degree/Course: Graduation(B.A)],
+          [University: M G K V P Varanasi Uttar Pradesh India], [Year: 2022]</b>
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in JavaScript Experienced with Vs Code Strong grasp of React Js
+          Excellent problem-solving skills Effective communicator and
+          collaborator
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Additional Certification
+        </h1>
+        <span>
+         <b> <ul>
+          <li>SQL foundation by DEV GYM Oracle</li>
+          <li> Python for biggener by Scaller</li>
+          <li>OOPS in JAVA by Great Learning Academy</li>
+          <li>Advance Deploma in Computer Application (A D C A)</li> 
+          <li>Course On Computer Concept (C C C)</li>
+          </ul></b>
+        </span>
+      
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative Web Development solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
